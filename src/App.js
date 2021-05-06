5edbf0b8b6f075ae220cec412f3be7285f1a737d
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-06 16:01:51 -0300

    substitui o componente home por um route que mapeia para home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Route exact path="/" component={Home} />
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
