27f153d1090d5e84bb4daa1a5f6bf5c6272e2f49
Huggo Parcelly <h.parcelly@gmail.com>
2021-05-06 15:59:02 -0300

    encapsula a app dentro do browserRouter e adiciona os links para navehaçao (exercicios 1 e 2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Home />
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+        </ul>
+      </BrowserRouter>
     );
   }
 }
